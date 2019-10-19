21411dd249867593ba99eabc6f7aec316e70ef4d
MashadZaib <mashadzaib.baber@gmail.com>
2019-10-20 00:17:44 +0500

    changed

--- restaurant/resources/views/restaurant-layout/footer.blade.php
+++ restaurant/resources/views/restaurant-layout/footer.blade.php
@@ -63,68 +63,68 @@
 
 
 
-    <script src="{{ URL::asset('public/assets/js/vendor.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/js/vendor.min.js')}}"></script>
 
 
 
     <!-- Third Party js-->
 
-    <script src="{{ URL::asset('public/assets/libs/peity/jquery.peity.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/peity/jquery.peity.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/apexcharts/apexcharts.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/jquery-vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/jquery-vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/jquery-vectormap/jquery-jvectormap-us-merc-en.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/jquery-vectormap/jquery-jvectormap-us-merc-en.js')}}"></script>
 
 
 
     <!-- Datatables js -->
 
-    <script src="{{ URL::asset('public/assets/libs/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/datatables/jquery.dataTables.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/datatables/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/datatables/dataTables.bootstrap4.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/datatables/dataTables.responsive.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/datatables/dataTables.responsive.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/datatables/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/datatables/dataTables.buttons.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/datatables/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/datatables/buttons.bootstrap4.min.js')}}"></script>
 
                 <!-- third party js ends -->
 
 
 
                 <!-- Datatables init -->
-    <script src="{{ URL::asset('public/assets/js/pages/datatables.init.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/js/pages/datatables.init.js')}}"></script>
 
 
 
-    <script src="{{ URL::asset('public/assets/libs/jquery-nice-select/jquery.nice-select.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/jquery-nice-select/jquery.nice-select.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/switchery/switchery.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/switchery/switchery.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/select2/select2.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/select2/select2.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/bootstrap-select/bootstrap-select.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/bootstrap-select/bootstrap-select.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/bootstrap-maxlength/bootstrap-maxlength.min.js')}}"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script> 
 
     <!-- Init js-->
-    <script src="{{ URL::asset('public/assets/js/pages/form-advanced.init.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/js/pages/form-advanced.init.js')}}"></script>
     <!-- Dashboard init -->
 
-    <script src="{{ URL::asset('public/assets/js/pages/dashboard-1.init.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/js/pages/dashboard-1.init.js')}}"></script>
     <!-- App js -->
 
-    <script src="{{ URL::asset('public/assets/js/app.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/js/app.min.js')}}"></script>
 
-    <script src="{{ URL::asset('public/assets/js/pages/tickets.js')}}"></script>
-    <script src="{{ URL::asset('public/assets/libs/jquery-toast/jquery.toast.min.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/js/pages/tickets.js')}}"></script>
+    <script src="{{ URL::asset('public/assets-restaurant/libs/jquery-toast/jquery.toast.min.js')}}"></script>
     <script type="text/javascript">
        
         function modal(header, body, footer, size, center, callback,classes) {
--- restaurant/resources/views/restaurant-layout/head.blade.php
+++ restaurant/resources/views/restaurant-layout/head.blade.php
@@ -7,40 +7,40 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="csrf-token" content="{{ Session::token() }}"> 
   <!-- App favicon -->
-  <link rel="shortcut icon" href="assets/images/favicon.ico">
+  <link rel="shortcut icon" href="assets-restaurant/images/favicon.ico">
   <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
   <!-- plugin css -->
-  <link href="{{asset('public/assets/libs/jquery-vectormap/jquery-jvectormap-1.2.2.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/jquery-vectormap/jquery-jvectormap-1.2.2.css')}}" rel="stylesheet">
   <!-- Plugins css -->
-  <link href="{{asset('public/assets/css/jquery.toast.min.css')}}" rel="stylesheet">
-   <link href="{{asset('public/assets/libs/jquery-toast/jquery.toast.min.css')}}" rel="stylesheet" type="text/css" />
-  <link href="{{asset('public/assets/libs/jquery-nice-select/nice-select.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/switchery/switchery.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/select2/select2.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/bootstrap-select/bootstrap-select.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/css/jquery.toast.min.css')}}" rel="stylesheet">
+   <link href="{{asset('public/assets-restaurant/libs/jquery-toast/jquery.toast.min.css')}}" rel="stylesheet" type="text/css" />
+  <link href="{{asset('public/assets-restaurant/libs/jquery-nice-select/nice-select.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/switchery/switchery.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/select2/select2.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/bootstrap-select/bootstrap-select.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.css')}}" rel="stylesheet">
 
 
 
   <!-- third party css -->
   
-  <link href="{{asset('public/assets/libs/datatables/dataTables.bootstrap4.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/datatables/responsive.bootstrap4.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/datatables/buttons.bootstrap4.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/libs/datatables/select.bootstrap4.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/datatables/dataTables.bootstrap4.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/datatables/responsive.bootstrap4.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/datatables/buttons.bootstrap4.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/datatables/select.bootstrap4.css')}}" rel="stylesheet">
   <!-- third party css end -->
-  <link href="{{asset('public/assets/libs/summernote/summernote-bs4.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/libs/summernote/summernote-bs4.css')}}" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.css" rel="stylesheet">
 
   <!-- App css -->
 
-  <link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/css/bootstrap.min.css')}}" rel="stylesheet">
   @yield('styles')
-  <link href="{{asset('public/assets/css/icons.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public/assets/css/app.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/css/icons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/assets-restaurant/css/app.min.css')}}" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
   
   
